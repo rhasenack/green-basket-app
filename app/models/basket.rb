7197d172460bb267cf94eb1ff88889e0bd352c20
Ricardo Hasenack <rhasenack@gmail.com>
2021-09-17 22:23:23 -0300

    added basic validations to restaurant and baskets

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -2,4 +2,7 @@ class Basket < ApplicationRecord
   has_many :tags
   has_many :orders, through: :orders_baskets
   belongs_to :restaurant
+
+  validates :name, length: { minimum: 5 }
+  validates :description, length: { minimum: 20 }
 end
