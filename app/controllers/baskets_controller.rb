7c015caf28f88de9b2b2cec734b2954e84f0c4de
Ricardo Hasenack <rhasenack@gmail.com>
2021-09-28 16:40:44 +0100

    implemented logic to display items in cart when reloading page

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -9,6 +9,7 @@ class BasketsController < ApplicationController
     @basket = Basket.find(params[:id])
     @restaurant = @basket.restaurant
     @baskets = Basket.where("restaurant_id = #{@restaurant.id} and id != #{@basket.id}")
+    @cart = Cart.where("user_id = #{current_user.id}").first
   end
 
   def edit
