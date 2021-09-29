5950a877b1688c9bde2455fea378dec469665cdf
Ricardo Hasenack <rhasenack@gmail.com>
2021-09-29 17:32:29 +0100

    created basic dashboard layout and added sample delete order methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,19 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "custom_sessions"}
 
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :baskets, only: [:index]
-
   resources :baskets, only: [:show] do
     member do
       get :edit
     end
   end
 
-  resources :orders, only: [:create]
+  resources :orders, only: [:create, :destroy]
 
   get '/cart/add', to: "carts#add_basket_to_cart"
   get '/cart/remove', to: "carts#remove_basket_from_cart"
   get '/cart/create', to: "carts#create_cart"
 
+  get '/user/dashboard', to: "user#dashboard"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_121039) do
+ActiveRecord::Schema.define(version: 2021_09_29_154605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2021_09_29_121039) do
     t.datetime "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
