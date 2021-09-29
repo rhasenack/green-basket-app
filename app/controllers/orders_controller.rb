27cb451ae41b21e2da86f12270ef864bb0be2bb8
Ricardo Hasenack <rhasenack@gmail.com>
2021-09-29 13:26:00 +0100

    implemented order creation. Pending redirect

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,15 +2,23 @@ class OrdersController < ApplicationController
 
 
   def create
-    @user_cart = Cart.where("user_id = #{current_user}").first
+    @user_cart = Cart.where("user_id = #{current_user.id}").first
     @cart_baskets = CartBasket.where("cart_id = #{@user_cart.id}")
     total_cart_price = 0
     ## Get Price
+
+    @order = Order.new(user: current_user, date: Time.now)
+    @order.save!
+
     @cart_baskets.each do |cart_basket|
-      total_cart_price += cart_basket.basket.price * cart_baskets.quantity
+      total_cart_price += cart_basket.basket.price * cart_basket.quantity
+      basket = cart_basket.basket
+      order_basket = OrdersBasket.new(order: @order, basket: basket, name: basket.name, description: basket.description, price: basket.price, quantity: cart_basket.quantity)
+      order_basket.save!
     end
 
-    @order = Order.new(user: current_user, price: total_cart_price, date: Time.now)
+    @order.price = total_cart_price
+    @order.save!
 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_144430) do
+ActiveRecord::Schema.define(version: 2021_09_29_121039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2021_09_28_144430) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "quantity"
     t.index ["basket_id"], name: "index_orders_baskets_on_basket_id"
     t.index ["order_id"], name: "index_orders_baskets_on_order_id"
   end
