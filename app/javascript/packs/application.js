d798a52bb8ef3bc6347bc06cdccb70bca3a9e7bd
Ricardo Hasenack <rhasenack@gmail.com>
2021-10-05 00:18:38 +0100

    added checkout js function to application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -35,7 +35,7 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMap();
-  // addToCheckout();
+  addToCheckout();
   enableEditButtons();
   autocompleteStart();
 });
