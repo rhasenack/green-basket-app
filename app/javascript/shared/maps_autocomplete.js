a5e41d2d76e8c6bb1500d0365c7d0de5d5ed2d39
Ricardo Hasenack <rhasenack@gmail.com>
2021-10-04 21:08:47 +0100

    added custom location to baskets index page with geocoder

--- app/javascript/shared/maps_autocomplete.js
+++ app/javascript/shared/maps_autocomplete.js
@@ -1,13 +1,15 @@
 const autocompleteStart = () => {
 
-  const initialize = () => {
-    var input = document.getElementById('restaurant_address');
-    new google.maps.places.Autocomplete(input);
+  if (document.querySelector('.restaurants-new')) {
+
+    const initialize = () => {
+      var input = document.getElementById('restaurant_address');
+      new google.maps.places.Autocomplete(input);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
   }
-
-  google.maps.event.addDomListener(window, 'load', initialize);
-
-
 }
 
 export {autocompleteStart}
