755b75b7888a44fd870bb8a8c7c519880ed19aea
Ricardo Hasenack <rhasenack@gmail.com>
2021-10-05 12:47:43 +0100

    fixed word wrapping in basket cards

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -10,4 +10,11 @@ class Basket < ApplicationRecord
 
   validates :name, length: { minimum: 5 }
   validates :description, length: { minimum: 20 }
+  validate :pickup_max_bigger_than_min, on: :create
+
+  def pickup_max_bigger_than_min
+    if pickup_max <= pickup_min
+      errors.add(:pickup_max, "Pickup Max has to be bigger than Picku Min")
+    end
+  end
 end
