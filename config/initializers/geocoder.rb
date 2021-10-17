6f3db3af53c868972a5ce96290f5fbf411541e60
Ricardo Hasenack <rhasenack@gmail.com>
2021-10-17 18:50:51 +0100

    added google api key to geocoder

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,6 +2,8 @@ Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
   lookup: :google,         # name of geocoding service (symbol)
+  api_key:   ENV['GOOGLE_API_SERVER_KEY'],
+  use_https: true,
   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
