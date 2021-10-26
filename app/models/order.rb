dddf120fb49fbde0663fa3fa58c46907a77c6db4
Ricardo Hasenack <rhasenack@gmail.com>
2021-10-26 16:38:12 +0100

    specs and validations for orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,19 @@
 class Order < ApplicationRecord
+  after_save :set_status
+
   belongs_to :user
   has_many :orders_baskets, dependent: :destroy
   has_many :baskets, through: :orders_baskets
+
+  validates :user, presence: true
+  validates :price, presence: true
+  validates :date, presence: true
+
+  validates :user, uniqueness: { scope: :date}
+
+  private
+
+  def set_status
+    update_column(:status, 'placed')
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:order) { Order.new(user_id: User.last.id, date: Time.now.to_date, price: 10) }
+  it 'is valid with a user, price and a date' do
+    expect(order).to be_valid
+  end
+  it 'has the status `paced` when is created' do
+    order.save
+    expect(Order.last.status).to eq('placed')
+  end
+  it 'is not valid without a price or without a date' do
+    price = order.price
+    order.price = nil
+    expect(order).not_to be_valid
+
+    order.price = price
+    order.date = nil
+    expect(order).not_to be_valid
+    order.date = Time.now.to_date
+  end
+
+  it 'can only exist one order per user per date' do
+    order.save
+    order2 = Order.new(user_id: User.last.id, date: Time.now.to_date, price: 20)
+    expect(order2).not_to be_valid
+  end
+
+
 end
