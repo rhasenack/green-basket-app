529969cdd9529cba4943050c0131ecd7ec17e22e
Ricardo Hasenack <rhasenack@gmail.com>
2021-10-26 11:31:47 +0100

    added tag specs

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,5 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_baskets
   has_many :baskets, through: :cart_baskets
+  validates :user_id, uniqueness: true
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:tag) { Tag.new(name: 'test tag') }
+  it 'is valid with a name' do
+    expect(tag).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    tag.name = nil
+    expect(tag).not_to be_valid
+  end
 end
